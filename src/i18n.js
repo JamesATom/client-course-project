950dd4122f468204153c85c46d8ba90dc03eaa15
Abdulaziz-Abduhakhimov <abduhakimovabdulaziz7@gmail.com>
2023-05-05 21:50:09 +0500

    removed api key string

--- src/i18n.js
+++ src/i18n.js
@@ -2,7 +2,6 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-locize-backend';
-// import { locizePlugin } from 'locize';
 import LastUsed from 'locize-lastused';
 
 const locizeOptions = {
@@ -40,6 +39,5 @@ export default i18n;
 //     projectId: process.env.REACT_APP_PROJECT_ID,
 //     apiKey: process.env.REACT_APP_API_KEY_LOCALIZE
 // }
-// "sync": "locize sync --path ./public/locales --project-id 489bdf18-d866-4726-b1e4-4914cb59a84a --api-key 783c8248-244d-473c-bf81-c79b71851114 --reference-language-only false"
-  
+
 
